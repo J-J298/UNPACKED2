991e5c9694cda6da54d0faae534ca4bb41c0fe98
J-J298 <113114388+J-J298@users.noreply.github.com>
2023-05-24 10:20:24 +0100

    New change
    updated map

---
+++ Assets/PanelOpener.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PanelOpener : MonoBehaviour {
+
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
